5861b688e92e47043367abae48f5bc855f467424
You Name <tadeudanilo46@gmail.com>
2020-01-21 01:30:18 -0300

    criando uma aplicação React Native Mobile.

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo</Text>
+      <Text style={styles.title}>App Github</Text>
+      <Text style={styles.content}>Desenvolvido por Neide</Text>
     </View>
   );
 }
@@ -12,8 +13,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0aa',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 50,
+    color: '#FFF',
+    fontWeight: "bold",
+  },
+
+  content: {
+    color: '#FFF',
+  }
 });
