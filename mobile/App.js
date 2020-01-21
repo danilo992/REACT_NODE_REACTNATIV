be9cb54f800456790e81be7d2716c44aa9649a5c
You Name <tadeudanilo46@gmail.com>
2020-01-21 18:45:20 -0300

    instalando as dependências expo maps, location, WebView.

--- mobile/App.js
+++ mobile/App.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+import Routes from './src/routes';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>App Github</Text>
-      <Text style={styles.content}>Desenvolvido por Neide</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7D40E7"  />
+      <Routes />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0aa',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 
-  title: {
-    fontSize: 50,
-    color: '#FFF',
-    fontWeight: "bold",
-  },
 
-  content: {
-    color: '#FFF',
-  }
-});
